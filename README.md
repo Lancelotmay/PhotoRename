98dad1391ee7318ebfd45be577f66e01277d4ed1
Lancelotmay <lancelotmay@gmail.com>
2019-05-26 15:51:04 +0800

    initial version

---
+++ README.md
@@ -0,0 +1,20 @@
+# photo_rename
+a script to rename photos using python
+
+## Feature
+Rename all photos (.jpg, .raw) in given dir (including sub dir)by definition:
+YYYYMMDD_HHMMSS_camera model_CRC32
+The infos are extract from exif.
+
+## Notes:
+
+- No action will taken if there is no exif info
+- Python 3 used
+
+## dependency 
+
+- filehash
+- exifread
+
+
+
---
+++ photo_rename.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 18 21:22:05 2019
+
+@author: Lancelot May
+
+photo_rename
+Copyright (C) 2018 Lancelot May lancelotmay@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import os, exifread
+from datetime import datetime
+from filehash import FileHash
+
+
+
+def MD5_16_calc(f):
+    md5hasher = FileHash('md5')
+    hash_l = md5hasher.hash_file(f.name)
+    return str(hash_l)[-4:]
+
+def FileNameGen (fname):
+    # Open image file for reading (binary mode)
+    # Return Exif tags
+    tags = exifread.process_file(f, details=False)
+    try:
+        date = str(tags['EXIF DateTimeOriginal'])
+    except KeyError:
+        return 'No EXIF info'
+    date = date.replace(":","")
+    date = date.replace(" ","_")
+    try:
+        model = str(tags['Image Model'])
+    except KeyError:
+        model = str('unknown')
+    model = model.replace(" ","")
+    MD5_16_str = MD5_16_calc(f)
+    NewName = date + '_'+model+'_'+MD5_16_str
+    return NewName
+
+
+## main function
+crc32hasher = FileHash('crc32')
+CurrentDir = '.'
+PhotoFileType = set(['jpg','jpeg','arw'])
+
+#open log file
+LogFileName = 'log.txt'
+LogFile = open(LogFileName, 'at', encoding='utf-8')
+LogFile.write('start changing name ')
+LogFile.writelines(str(datetime.today()) + '\n')
+
+for root, dirs, files in os.walk(CurrentDir, topdown=False):
+    for name in files:
+      fname = (os.path.join(root, name))
+      fext = name.split('.')[-1].lower()
+      if fext in PhotoFileType:
+          f = open(fname, 'rb')
+          NameGen= FileNameGen (f)
+          NameFull = NameGen + '.'+fext
+          NewName = (os.path.join(root, NameFull))
+          f.close()
+          if NameGen == 'No EXIF info':
+              LogFile.writelines("ERROR " + fname + ' : ' + NameGen + '\n')
+          elif os.path.isfile(NewName):
+              LogFile.writelines("ERROR " + fname +" to " + NewName +':  file exist'  + '\n')
+          else:
+              os.rename(f.name,NewName)
+              LogFile.writelines("rename " + fname +" to " + NewName + '\n')
+          
+              
+
+LogFile.close()
+  
+      
+         
